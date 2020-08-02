3d76aea4517f90cfdf7cbc73dbb83c4a57659838
Ethan <ethan_gustafson@outlook.com>
2020-08-01 22:18:35 -0400

    added comments clarifying that the original string was being modified in blanket_pattern.rb

--- Blanket_Pattern_Ruby_JS_Algorithms/blanket_pattern.rb
+++ Blanket_Pattern_Ruby_JS_Algorithms/blanket_pattern.rb
@@ -22,11 +22,23 @@ string = "RRGGBBYYKK"
 #     new_string.concat(character)
 # end
 
-20.times do
+20.times do # Everything between do and end is a code block. Which means it has its own scope.
     puts string
     character = string.slice!(0)
     string.concat(character)
-end
+    puts "This is the block scoped variable string: #{string}" 
+end # although it has its own scope, it is still destructively modifying the original string.
+
+#uncomment the code below, comment out 20.times iteration above and you will see that it does in fact modify the original string.
+
+# 21.times do
+#     puts string
+#     character = string.slice!(0)
+#     string.concat(character)
+#     puts "This is the block scoped variable string: #{string}" 
+# end
+
+# puts "This is the original variable string: '#{string}'', which should be equal to 'RRGGBBYYKK'" 
 
 # What does slice do? It deletes the first letter of the string and gives it back as the return value.
 # How is .slice and .slice! different? slice removes it non-destructively, slice will modify the original string.
