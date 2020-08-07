87b1bdf3b74e696b2a3e1483e462e632487dc9c2
Ethan <ethan_gustafson@outlook.com>
2020-08-07 04:44:17 -0400

    fixed list_file edit method, now functional

--- List_Maker/lib/list_file.rb
+++ List_Maker/lib/list_file.rb
@@ -27,7 +27,7 @@ module ListMaker
         # Read from list file
         # Use File.new/open in read mode
         File.open(@@filepath, 'r') do |f|
-          i = 0
+          i = 1
           f.each_line do |line|
             puts "#{i}: #{line}"
             i += 1
@@ -36,63 +36,37 @@ module ListMaker
         end
       end
       
-      def add(args=[])
+      def add
         puts "\nAdd a List Item\n\n".upcase
         # Add item to the end of the list.
         # Use File.new/open in append mode
+        puts "Enter the new list item and hit Return: "
+        response = gets.chomp
+
         File.open(@@filepath, 'a') do |f|
-          if args.is_a?(Array)
-            args.each do |a|
-              f.puts a
-            end
-          else
-            splitted = args.split(" ")
-            splitted.each do |a|
-              f.puts a
-            end
-          end
+          f.puts response
         end
       end
       
-      def edit(args=[])
+      def edit(number)
         puts "\nEdit a List Item\n\n".upcase
         # get the item position from the args ("edit 3", "edit 7")
-        # position = args.first.to_i
+        position = number.first.to_i - 1
         # read list file and make sure that item exists
         #   Use File.readlines
         array = File.readlines(@@filepath)
-        file = File.read(@@filepath)
+        if array[position] == nil
+          puts "No Item Found"
+        else
+          puts "You have selected: #{array[position]}"
+          puts "To edit, enter the new text and hit Return: "
+        response = gets
 
-        args.each do |arg|
-          if !array.find{|a| a == array[arg]}
-
-            puts "Item Not Found"
-            self.view
-  
-            puts "Add an item?\n"
-            new_arg = gets.chomp
-            self.add(new_arg)
-            abort
-
-          else
-
-            File.open(@@filepath, 'r') do |f|
-              i = 0
-              f.each_line do |line|
-                puts "#{i}: #{line}"
-                i += 1
-                # add numbers next to list items
-              end
-            end
-
-            print "#{arg}: "
-            edit_item = gets.chomp
-            array[arg] = edit_item
-
-            data = array.join
-            File.write(@@filepath, data)
-          end
+        array[position] = response
+        new_data = array.join
+        File.write(@@filepath, new_data)
         end
+      
         # return not found message if item does not exist
         # output text of current list item again
         # ask user to type new text
