9bd7dbe7e1b374789af5cbe92996cc237a586f42
Ethan <ethan_gustafson@outlook.com>
2020-08-07 15:18:37 -0400

    added barebones mail merge project

---
+++ Mail_Merge/init.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+#### Mail Merge ####
+#
+# Launch this Ruby file from the command line
+# to get started
+# 
+
+APP_ROOT = File.expand_path(File.dirname(__FILE__))
+
+# Read a template file containing the text of a letter
+
+# Read a CSV file to get an array of people
+
+# Set instance variables to data for each person
+
+# Use ERB to bind instance variables to the letter template
+
+# Save the results as separate files in a directory (e.g., "letter_01.txt")
